7674f4f9a22755411ad2bd1b4afb81540aaf2882
kiouji123 <51143525+kiouji123@users.noreply.github.com>
2020-08-28 18:15:49 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,6 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
-
+from sklearn.utils import resample
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -15,6 +15,32 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+  
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class Render(BaseEstimator, TransformerMixin):
+    def __init__(self, col):
+        self.col = col
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        df_majority = data[data.OBJETIVO=='Aceptado']
+        df_minority = data[data.OBJETIVO=='Sospechoso']
+        df_minority_upsampled = resample(df_minority, 
+                                 replace=True,     # sample with replacement
+                                 n_samples=8873,    # to match majority class
+                                 random_state=12)
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return  pd.concat([df_majority, df_minority_upsampled])
+  
+    
+    
+    
+    
     
     
 
