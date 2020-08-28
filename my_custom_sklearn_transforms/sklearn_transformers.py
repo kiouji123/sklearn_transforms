a02dfd1b4758174fc6c1faa56906c867d5b07bf6
kiouji123 <51143525+kiouji123@users.noreply.github.com>
2020-08-28 14:55:05 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -29,4 +29,4 @@ class DropRow(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.dropna(how='any',axis=numero)
+        return data.dropna(how='any',axis=self.numero)
